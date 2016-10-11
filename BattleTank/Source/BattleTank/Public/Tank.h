33a24441fddf5be2f0a2b61781000210606cfadb
Ashengar <sdmarett@gmail.com>
2016-10-11 10:14:37 +0100

    BT_ Limiting Projectile Fire Rate

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -43,12 +43,21 @@ private:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
 
-	UPROPERTY(EditAnyWhere, Category = Firing)
-	float LaunchSpeed = 4000;
-	
-	UPROPERTY(EditAnyWhere, Category = Setup)
+	UPROPERTY(EditDefaultsOnly, Category = Setup)
 	TSubclassOf<AProjectile> ProjectileBlueprint;
+
+	UPROPERTY(EditDefaultsOnly, Category = Firing)
+	float LaunchSpeed = 4000;
+
+	UPROPERTY(EditDefaultsOnly, Category = Firing)
+	float ReloadTimeInSeconds = 3;
+	
+
 
 	// Local barrel ref for spawning projectile
 	UTankBarrel* Barrel = nullptr;
+
+	
+
+	double LastFireTime = 0;
 };
